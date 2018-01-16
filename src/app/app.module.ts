06a1e5a6a8298d5d059515fffa8d33fa16ed24eb
Jean-Pierre <Olwagenjp@gmail.com>
2018-01-16 13:34:49 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './components/Employee/employee.component';
+import { EmployeeDetailComponent } from './components/EmployeeDetail/employee-detail.component';
+import { AppRoutingModule } from './routing.module';
+import { EmployeeService } from './services/employee.service';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { NavigationComponent } from './components/Navigation/nav.component';
+import { EmployeeListComponent } from './components/EmployeeList/EmployeeList.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    EmployeeComponent,
+    EmployeeDetailComponent,
+    NavigationComponent,
+    EmployeeListComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent],
 
 
